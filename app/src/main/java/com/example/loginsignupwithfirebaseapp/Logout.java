047468264385718f43220aefe70a80ddec7c1aa2
github <pragya.sharma2_cs19@gla.ac.in>
2021-10-24 20:39:39 +0530

    add toast.

--- app/src/main/java/com/example/loginsignupwithfirebaseapp/Logout.java
+++ app/src/main/java/com/example/loginsignupwithfirebaseapp/Logout.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 
@@ -22,6 +23,7 @@ public class Logout extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 f.signOut();
+                Toast.makeText(Logout.this, "Logout successfully", Toast.LENGTH_SHORT).show();
                 Intent i = new Intent(Logout.this, MainActivity.class);
                 startActivity(i);
                 finish();
