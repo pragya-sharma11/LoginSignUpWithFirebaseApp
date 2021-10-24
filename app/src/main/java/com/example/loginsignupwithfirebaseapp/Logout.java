6e447df2aa5ef15ae3848cc890f3a412948f20bb
github <pragya.sharma2_cs19@gla.ac.in>
2021-10-24 20:24:42 +0530

    make functionality of logout button.

--- app/src/main/java/com/example/loginsignupwithfirebaseapp/Logout.java
+++ app/src/main/java/com/example/loginsignupwithfirebaseapp/Logout.java
@@ -2,13 +2,30 @@ package com.example.loginsignupwithfirebaseapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class Logout extends AppCompatActivity {
-
+    Button b;
+    FirebaseAuth f;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_logout);
+        b = findViewById(R.id.button);
+        f = FirebaseAuth.getInstance();
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                f.signOut();
+                Intent i = new Intent(Logout.this, MainActivity.class);
+                startActivity(i);
+                finish();
+            }
+        });
     }
 }
