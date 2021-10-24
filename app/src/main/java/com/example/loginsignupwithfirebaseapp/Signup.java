10c4fa8a56aa4f8a8e8e5274bf628f86bac1eaaa
github <pragya.sharma2_cs19@gla.ac.in>
2021-10-24 20:10:42 +0530

    declare variables.

--- app/src/main/java/com/example/loginsignupwithfirebaseapp/Signup.java
+++ app/src/main/java/com/example/loginsignupwithfirebaseapp/Signup.java
@@ -2,9 +2,17 @@ package com.example.loginsignupwithfirebaseapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class Signup extends AppCompatActivity {
-
+    EditText e1, e2;
+    Button b1, b2;
+    ProgressBar p;
+    FirebaseAuth f1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
