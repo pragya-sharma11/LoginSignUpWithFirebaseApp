216edc94eebd1dc90caf998efd168b624d97bdad
github <pragya.sharma2_cs19@gla.ac.in>
2021-10-24 19:42:04 +0530

    declaration of variable for main activity.

--- app/src/main/java/com/example/loginsignupwithfirebaseapp/MainActivity.java
+++ app/src/main/java/com/example/loginsignupwithfirebaseapp/MainActivity.java
@@ -3,12 +3,21 @@ package com.example.loginsignupwithfirebaseapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
-
+    EditText e1, e2;
+    Button b1, b2;
+    ProgressBar p;
+    FirebaseAuth f1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 }
